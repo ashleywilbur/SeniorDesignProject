34f42f1d184a646ee2a0b2c03eeba0b511fb69a7
BrennanDE <BrennanDE@gmail.com>
2017-04-08 15:03:03 -0400

    Add files via upload

---
+++ Devyn Test/user.php
@@ -0,0 +1,62 @@
+<?php
+	// Create connection for secured servers
+	$servername = "localhost";
+	$username = "root";
+	$password = "password";
+	$dbname = "rmf_test_devyn";
+	
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	
+	// Create Connection for unsecured users
+	//$conn = new mysqli();
+	//mysql_select_db('movies');
+
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	} 
+	//echo "Connected successfully";
+	
+	
+	//display package count
+	
+	if(isset($_POST['email'])){
+		$email = $_POST['email'];
+	}
+	
+	
+	//$package = $_POST["package"];
+	
+	
+	//Checks Package ID and returns the current step
+	
+	$sql = "SELECT UID FROM user WHERE user.Email LIKE '%" . $email . "%'";
+	$result = $conn->query($sql);
+	
+	
+	if (!$result) {
+		trigger_error('Invalid query: ' . $conn->error);
+	}
+	
+	$row = mysql_fetch_row($result);
+	
+	
+	//output of UID
+	echo $row["UID"];
+	
+	
+	//reference code - not used
+	
+	/*$sql = "SELECT id, firstname, lastname FROM MyGuests";
+	$result = $conn->query($sql);
+
+	if ($result->num_rows > 0) {
+		// output data of each row
+		while($row = $result->fetch_assoc()) {
+			echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+		}
+	} else {
+		echo "0 results";
+	}*/
+	$conn->close();
+?>
