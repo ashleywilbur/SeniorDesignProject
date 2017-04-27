df3264a658ab23e118c2432017480632d1d062b1
DevynJamesRogers <devyn.j.rogers@gmail.com>
2017-04-26 22:26:09 -0400

    generate report

--- rmf_tracker/files/theme/GenerateReport.js
+++ rmf_tracker/files/theme/GenerateReport.js
@@ -8,83 +8,48 @@ window.onload = loadDoc;
 packNum = window.location.href.split("#") 
 
 console.log(packNum);
-function TrackerColor(id, color) {
-
-	//What shows up on first click
-	if(action == 1){
-			document.getElementById('T1').style.visibility = 'visible';
-
-	
-			action = 2;
-	}
-	//everything gets hidden on the second click
-	else{			
-			document.getElementById('T1').style.visibility = 'hidden';
-
-			
-			action= 1;
-
-	}
+function TrackerColor(id) {
+		
+		AA();
+		
+		if(id == "track1")
+			document.getElementById('Snum').innerHTML = "Step #1";
+		if(id == "track2")
+			document.getElementById('Snum').innerHTML = "Step #2";
+		if(id == "track3")
+			document.getElementById('Snum').innerHTML = "Step #3";
+		if(id == "track4")
+			document.getElementById('Snum').innerHTML = "Step #4";
+		if(id == "track5")
+			document.getElementById('Snum').innerHTML = "Step #5";
+		if(id == "track6")
+			document.getElementById('Snum').innerHTML = "Step #6";
+	document.getElementById('T1').style.visibility = 'visible';
 }
-function TrackClick(){
-		//What shows up on first click
-	if(action == 1){
-			document.getElementById('T1').style.visibility = 'visible';
-			document.getElementById('T1').innerHTML = "<h1>Step #2</h1>Step Description: <br>Deliver Date: <h3>Artifacts: </h3> <h3>Progress: </h3>";;
-			
-			
-			action = 2;
-	}
-	//everything gets hidden on the second click
-	else{			
-			document.getElementById('T1').style.visibility = 'hidden';
+function AA(){
+ var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+      response =this.responseText;  
 
-			
-			action= 1;
-
+	if(this.readyState == 3){
+	  if(response == 1)
+		document.getElementById("kickBack").innerHTML =  "<label> Delivered </label>\
+<button id=\"del\" class = \"artButton\"></button><br>\
+<form id=\"kickForm\" action = \"\" method = \"POST\">\
+	Step approved? <br>\
+	<input type=\"radio\" name=\"approved\" value=\"yes\"> Yes<br>\
+	<input type=\"radio\" name=\"approved\" value=\"no\"> No<br>\
+	Kickback Reason: <br>\
+	<input rows=\"1\" cols=\"50\" wrap=\"physical\" type=\"text\" name=\"kickback\"> <br>\
+	<button id=\"saveKickback\">Save </button><br>\
+</form> ";
 	}
+  };
+  xhttp.open("POST", "http://localhost/rmf_tracker/php_Files/userroles.php", true);
+  xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhttp.send("uid="+localStorage.getItem("UID"));
+
 }
-
-function TrackClick3(){
-		//What shows up on first click
-	if(action == 1){
-			document.getElementById('T1').style.visibility = 'visible';
-			document.getElementById('T1').innerHTML = "<h1>Step #3</h1>Step Description: <br>Deliver Date: <h3>Artifacts: </h3> <h3>Progress: </h3>";;
-
-			
-			
-			action = 2;
-	}
-	//everything gets hidden on the second click
-	else{			
-			document.getElementById('T1').style.visibility = 'hidden';
-
-			
-			action= 1;
-
-	}
-}
-
-function PackageDrop(){
-	
-		//What shows up on first click
-	if(action == 1){
-			document.getElementById('dropdown1').style.visibility = 'visible';
-
-			action = 2;
-	}
-	//everything gets hidden on the second click
-	else{			
-			document.getElementById('dropdown1').style.visibility = 'hidden';
-
-			
-			action= 1;
-
-	}
-	
-}
-
-
 	
 	
 
@@ -97,30 +62,28 @@ function loadDoc() {
       response =this.responseText;  
 	 // Information = response.split(","); Can be used to take in info as array instead of object
 	if(this.readyState == 3){
-	  console.log(response);
+	  
 	 Information = response;
-	 
+	 console.log(Information);
 	 //Parse brings errors but if it is not there it does not work
 	Information =JSON.parse(Information);
 	
 	//Brings everything to the webpage
-   	document.getElementById('Pname1').innerHTML = Information.Name;
-	document.getElementById('Pacronym1').innerHTML = "Package Acronym: "+Information.Acronym;
-	document.getElementById('Atype1').innerHTML = "Accredidation Type: "+Information.AccredType;	//wrong
-	document.getElementById('CATdate1').innerHTML = "Certification Acquired Target Date: "+Information.CIA;
-	document.getElementById('Descript1').innerHTML = Information.Description;
-	document.getElementById('Zone').innerHTML = "Zone: "+Information.Zone; //wrong
-
-	
+   	document.getElementById('Pname').innerHTML = Information.Name;
+	//document.getElementById('infoTest').innerHTML = Information;
+	document.getElementById('Pacronym').innerHTML = "Package Acronym: "+Information.Acronym;
+	document.getElementById('Atype').innerHTML = "Accredidation Type: "+Information.AccredType;
+	document.getElementById('CATdate').innerHTML = "Certification Acquired Target Date: "+Information.CIA;
+	document.getElementById('Sdesc').innerHTML = Information.Description;
+	document.getElementById('Zone').innerHTML = "Zone: "+Information.Zone;	
 	//loop for tracker color
 	  for(i =1; i < Information.TrackerStep; i++){
 		  document.getElementById("Track"+i).style.backgroundColor = '#63AC50';
 		  document.getElementById("Track"+(i+1)).style.backgroundColor = '#FFFF69';
 	  }
 	}
-  }; console.log(loadDoc);
-  
-  xhttp.open("POST", "http://localhost/rmf_tracker/phpFiles/tracker.php", true);
+  };
+  xhttp.open("POST", "http://localhost/rmf_tracker/php Files/tracker.php", true);
   xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
   xhttp.send("package="+packNum[1]);
 
