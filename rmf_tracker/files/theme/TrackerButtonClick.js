101764c451a74c68ab12d4d81f64850faa81eb12
DevynJamesRogers <devyn.j.rogers@gmail.com>
2017-04-27 15:30:27 -0400

    trackerupdate

--- rmf_tracker/files/theme/TrackerButtonClick.js
+++ rmf_tracker/files/theme/TrackerButtonClick.js
@@ -12,18 +12,78 @@ function TrackerColor(id) {
 		
 		AA();
 		
-		if(id == "track1")
-			document.getElementById('Snum').innerHTML = "Step #1";
-		if(id == "track2")
-			document.getElementById('Snum').innerHTML = "Step #2";
-		if(id == "track3")
-			document.getElementById('Snum').innerHTML = "Step #3";
-		if(id == "track4")
-			document.getElementById('Snum').innerHTML = "Step #4";
-		if(id == "track5")
-			document.getElementById('Snum').innerHTML = "Step #5";
-		if(id == "track6")
-			document.getElementById('Snum').innerHTML = "Step #6";
+if(id == "track1"){
+			document.getElementById('T1').innerHTML = 
+
+'<br><br><h1 id = "Snum" >Step #1</h1>'+
+'<h3>Artifact Progress</h3> <br>'+
+'<form action="artifactsubmit.php" method="POST">'+
+'<!--name is paid-->'+
+'<label> Artifact 1 </label>'+
+'<button id="art1" class = "artButton"></button>'+
+'<input class="artSub" type="checkbox" name="art1sub" value="artifactsubmit"> Submit &nbsp'+
+'<input class="artApp"type="checkbox" name="art1app" value="artifactapproved"> Approve &nbsp <br>'+
+'<label> Artifact 2 </label>'+
+'<button id="art2" class = "artButton"></button>'+
+'<span class="artSub"> <input type="checkbox" name="art2sub" value="artifactsubmit"> Submit &nbsp </span>'+
+' <input class="artApp" type="checkbox" name="art2app" value="artifactapproved"> Approve &nbsp <br>'+
+'<label> Artifact 3 </label>'+
+'<button id="art3" class = "artButton"></button>'+
+'<span class="artSub"><input class="artSub" type="checkbox" name="art3sub" value="artifactsubmit"> Submit &nbsp </span>'+
+' <input class="artApp" type="checkbox" name="art3app" value="artifactapproved"> Approve &nbsp <br><br> <br>'+
+'<label> Delivered </label>'+
+'<button id="del" class = "artButton"></button><br>'+
+'<div class="kickBack" id="kickBack">'+
+'</div>'+
+'</form>'+
+
+'<div class="kickBack" id="kickBack">'+
+
+'<form action = "" method = "POST">'+
+	'Step approved? <br>'+
+	'<input type="radio" name="approved" value="yes"> Yes<br>'+
+	'<input type="radio" name="approved" value="no"> No<br>'+
+	'Kickback Reason: <br>'+
+	'<input type="text" name="kickback"> <br>'+
+'	<button id="saveKickback">Save </button><br>'+
+'</form>';
+}/*
+
+		if(id == "track2"){
+			document.getElementById('T1').style.visibility = 'hidden';
+			document.getElementById('T2').style.visibility = 'visible';
+			document.getElementById('T3').style.visibility = 'hidden';
+			document.getElementById('T4').style.visibility = 'hidden';
+			document.getElementById('T5').style.visibility = 'hidden';
+		document.getElementById('T6').style.visibility = 'hidden';}
+		if(id == "track3"){
+			document.getElementById('T1').style.visibility = 'hidden';
+			document.getElementById('T2').style.visibility = 'hidden';
+			document.getElementById('T3').style.visibility = 'visible';
+			document.getElementById('T4').style.visibility = 'hidden';
+			document.getElementById('T5').style.visibility = 'hidden';
+		document.getElementById('T6').style.visibility = 'hidden';}
+		if(id == "track4"){
+			document.getElementById('T1').style.visibility = 'hidden';
+			document.getElementById('T2').style.visibility = 'hidden';
+			document.getElementById('T3').style.visibility = 'hidden';
+			document.getElementById('T4').style.visibility = 'visible';
+			document.getElementById('T5').style.visibility = 'hidden';
+		document.getElementById('T6').style.visibility = 'hidden';}
+		if(id == "track5"){
+			document.getElementById('T1').style.visibility = 'hidden';
+			document.getElementById('T2').style.visibility = 'hidden';
+			document.getElementById('T3').style.visibility = 'hidden';
+			document.getElementById('T4').style.visibility = 'hidden';
+			document.getElementById('T5').style.visibility = 'visible';
+		document.getElementById('T6').style.visibility = 'hidden';}
+		if(id == "track6"){
+			document.getElementById('T1').style.visibility = 'hidden';
+			document.getElementById('T2').style.visibility = 'hidden';
+			document.getElementById('T3').style.visibility = 'hidden';
+			document.getElementById('T4').style.visibility = 'hidden';
+			document.getElementById('T5').style.visibility = 'hidden';
+		document.getElementById('T6').style.visibility = 'visible';}*/
 	document.getElementById('T1').style.visibility = 'visible';
 }
 function AA(){
@@ -32,18 +92,10 @@ function AA(){
       response =this.responseText;  
 
 	if(this.readyState == 3){
-	  //if(response == 1){
-		document.getElementById("kickBack").innerHTML =  "<label> Delivered </label>\
-<button id=\"del\" class = \"artButton\"></button><br>\
-<form id=\"kickForm\" action = \"\" method = \"POST\">\
-	Step approved? <br>\
-	<input type=\"radio\" name=\"approved\" value=\"yes\"> Yes<br>\
-	<input type=\"radio\" name=\"approved\" value=\"no\"> No<br>\
-	Kickback Reason: <br>\
-	<input rows=\"1\" cols=\"50\" wrap=\"physical\" type=\"text\" name=\"kickback\"> <br>\
-	<button id=\"saveKickback\">Save </button><br>\
-</form> ";
-	//}
+		
+	  if(response == 1){
+
+	}
 	}
   };
   xhttp.open("POST", "http://localhost/rmf_tracker/php_Files/userroles.php", true);
@@ -75,7 +127,14 @@ function loadDoc() {
 	document.getElementById('Pacronym').innerHTML = "Package Acronym: "+Information.Acronym;
 	document.getElementById('Atype').innerHTML = "Accredidation Type: "+Information.AccredType;
 	document.getElementById('CATdate').innerHTML = "Certification Acquired Target Date: "+Information.CIA;
-	document.getElementById('Sdesc').innerHTML = Information.Description;
+	//document.getElementById('Sdesc').innerHTML = Information.Description;
+	
+	for(var i=1; i <= Information.i;i++){
+	var aName = i.toString();
+	aName = "ArtifactName"+aName;
+	//	artifacts(Information.ArtifactName+i);
+	console.log(Information[aName]);
+	}
 	
 	//loop for tracker color
 	  for(i =1; i < Information.TrackerStep; i++){
@@ -90,4 +149,10 @@ function loadDoc() {
 
  
    
+}
+
+function artifacts(aName, stepID){
+	
+	
+	
 }
